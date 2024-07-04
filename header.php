e0eaad2feec20d15610f3e9844f6905148015458
Sonixxx <jason.suaybaguio@formstack.com>
2024-07-05 05:48:49 +0800

    --test
    tttt

---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="PH">
+  <head>
+    <meta charset="utf-8">
+    <title>Innovative Technological College of the Philippines</title>
+    <link rel="icon" href="img/getintopc.ico">
+    <style type="text/css">
+
+    @media screen and (max-width:990px){
+    	#vis{
+    		margin-top: 10px
+    	}   }
+
+
+      .ii{
+      	font-size: 30px
+      }
+      .iii{
+      	font-size: 23px
+      }
+
+    @media screen and (max-width:350px){
+    	.ii{
+    		font-size: 24px
+    	}
+    	.iii{
+    		font-size: 16px
+    	}
+
+      }
+
+
+    @media screen and (max-width: 360px){
+        .them{
+          padding-top: 20px
+        }
+
+      }
+
+      #tblc{
+        width:50%;
+        margin-left:auto;
+        margin-right:auto;
+      }
+      @media screen and (max-width: 767px){
+          #tblc{
+          width:100%;
+        }
+      }
+       @media screen and (max-width: 325px){
+          #cr1{
+          padding-bottom: 2px
+        }
+      }
+
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <meta name="author" content="Innotech">
+    <meta name="keywords" content="newgetintopc.com, newgetintopc, getintopc.com, Getintopc, Free software download website, Latest Software Download, Learn Computer Tips, Fix PC Issues, tutorials, performance, tricks to solve problems in computer " >
+
+    <meta name="description" content="Latest Software Download. Learn Computer Tips, Fix PC Issues, tutorials and performance tricks to solve problems.. " >
+    <!-- Google Analytics -->
+
+    <!-- End Google Analytics -->
+
+    <!--Google Adsense sript below-->
+       <script data-ad-client="ca-pub-3995015911667837" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!--Google Adsense sript above-->
+
+    <!-- Favicon -->
+    <link href="img/getintopc.ico" rel="icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700|Roboto:400,900" rel="stylesheet">
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Insert to your webpage before the </head> -->
+     <link rel="stylesheet" type="text/css" href="js/sliderengine/amazingslider-1.css">
+     <link rel="stylesheet" href="lib/w/w3.css">
+    <!-- =======================================================
+      Theme Name: Inno-Tech
+      Author: Innovator's Group
+    ======================================================= -->
+  </head>
